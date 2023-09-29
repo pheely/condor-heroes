4c03ce007b6abe6021f45da037b71d02f44a369f
Philip Yang <philip.yang@scotiabank.com>
2023-09-29 03:36:19 +0000

    modified employee api

--- run/employee-api/internal/handler/service.go
+++ run/employee-api/internal/handler/service.go
@@ -1,14 +1,16 @@
 package handler
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
-	"github.com/rs/zerolog/log"
 	"github.com/gorilla/mux"
 	"github.com/pheely/employee-api/internal/stores"
+	"github.com/rs/zerolog/log"
 )
 
 func JsonHeader(next http.Handler) http.Handler {
@@ -24,22 +26,32 @@ type Service struct {
 }
 
 type employee struct {
-	Id	   		string `json:"id"`
-	First_Name 	string `json:"first_name"`
-	Last_Name  	string `json:"last_name"`
-	Department 	string `json:"department"`
-	Salary    	int    `json:"salary"`
-	Age      	int    `json:"age"`
+	Id         string `json:"id"`
+	First_Name string `json:"first_name"`
+	Last_Name  string `json:"last_name"`
+	Department string `json:"department"`
+	Salary     int    `json:"salary"`
+	Age        int    `json:"age"`
+}
+
+type PubSubNotification struct {
+	Message struct {
+		Attributes  map[string]interface{} `json:"attributes"`
+		MessageID   string                 `json:"messageId"`
+		PublishTime time.Time              `json:"publishTime"`
+		Data        string                 `json:"data"`
+	} `json:"message"`
+	Subscription string `json:"subscription"`
 }
 
 func convertToEmployeeJson(t stores.Employee) employee {
 	jsonT := employee{
-		Id:				t.ID,
-		First_Name:     t.First_Name,
-		Last_Name: 		t.Last_Name,
-		Department:     t.Department,
-		Salary:       	t.Salary,
-		Age: 			t.Age,
+		Id:         t.ID,
+		First_Name: t.First_Name,
+		Last_Name:  t.Last_Name,
+		Department: t.Department,
+		Salary:     t.Salary,
+		Age:        t.Age,
 	}
 	return jsonT
 }
@@ -138,4 +150,48 @@ func (s *Service) CreateEmployee(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	json.NewEncoder(w).Encode(convertToEmployeeJson(t))
+}
+
+func (s *Service) ProcessCreateEvent(w http.ResponseWriter, r *http.Request) {
+	// body, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	// just log the error and return
+	// 	log.Error().Err(err).Msg("Error reading POST data")
+	// 	return
+	// }
+	// log.Debug().Msg("POST data =" + string(body))
+	// r.Body.Close()
+
+	// if len(bytes.TrimSpace(body)) == 0 {
+	// 	log.Warn().Msg("Empty request body. Expecting a PubSub message.")
+	// 	return
+	// }
+
+	decoder := json.NewDecoder(r.Body)
+	var notification PubSubNotification
+	err := decoder.Decode(&notification)
+	if err != nil {
+		log.Warn().Err(err).Msg("Error unmarshalling POST data")
+		return
+	}
+
+	decoded, error := base64.StdEncoding.DecodeString(notification.Message.Data)
+	if error != nil {
+		log.Warn().Err(error).Msg("decoding base64 encoded data: " + notification.Message.Data)
+		return
+	}
+
+	log.Debug().Msg("received: " + string(decoded))
+
+	var payload stores.Employee
+	err = json.Unmarshal(decoded, &payload)
+	if err != nil {
+		log.Warn().Err(error).Msg("Couldn't unmarshal pub/sub message payload")
+		return
+	}
+	err = s.Connection.Create(&payload)
+	if err != nil {
+		log.Error().Err(error).Msg("Failed to create employee")
+		return
+	}
 }
