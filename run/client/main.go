8137849f659155391cdaf5826ed176971a582c31
Philip Yang <philip.yang@scotiabank.com>
2023-09-29 05:07:26 +0000

    modified employee api

--- run/client/main.go
+++ run/client/main.go
@@ -1,20 +1,27 @@
 package main 
 
 import (
+    "context"
     "fmt"
     "io"
     "log"
     "net/http"
     "os"
+
+    "cloud.google.com/go/compute/metadata"
+    "google.golang.org/api/idtoken"
 )
 
-    
-func main() {
+var employeeApiUrl string
+
+func init() {
     employeeApiUrl := os.Getenv("EMPLOYEE_API")
     if employeeApiUrl == ""  {
         log.Fatal("Employ API URL is required")
     }
+}
 
+func main() {
     http.HandleFunc("/", handler)
     log.Printf("Listening on port 8080")
     err := http.ListenAndServe(":8080", nil)
@@ -23,10 +30,15 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     log.Println("Serving request")
-
-    employeeApiUrl := os.Getenv("EMPLOYEE_API")
     
-        resp, err := http.Get(employeeApiUrl + "/api/help")
+    if metadata.OnGCE()  {
+	log.Println("OnGCE: true")
+    	// employeeApiUrl is the audience
+    	client, err := idtoken.NewClient(context.Background(), employeeApiUrl)
+    	if err != nil {
+    	    log.Fatal("Failed to get idtoken")
+    	}
+        resp, err := client.Get(employeeApiUrl + "/api/employee")
         if err != nil {
 	    log.Fatal("Failed calling Employee API")
         }
@@ -41,4 +53,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
     
         w.WriteHeader(http.StatusOK)
         w.Write(body)
+    } else {
+        resp, err := http.Get(employeeApiUrl + "/api/employee")
+        if err != nil {
+	    log.Fatal("Failed calling Employee API")
+        }
+        defer resp.Body.Close()
+
+        body, err := io.ReadAll(resp.Body)
+        if err != nil {
+    	    log.Fatal("Failed read response from Employee API")
+        }
+
+        fmt.Println("Response from Employee API: ", string(body))
+    
+        w.WriteHeader(http.StatusOK)
+        w.Write(body)
+    }
 }
