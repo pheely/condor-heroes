41149876429d9699d43acf8ddbd8985be06a01b1
Philip Yang <philip.yang@scotiabank.com>
2023-11-29 07:27:03 +0000

    real uat secret

--- vault/vault-access/src/main/java/com/bns/ts/vault/App.java
+++ vault/vault-access/src/main/java/com/bns/ts/vault/App.java
@@ -57,6 +57,44 @@ public class App implements CommandLineRunner {
     }
 
     logger.info("Access granted");
+
+//    readFromUAT();
   }
 
+  /*
+  This call fails. It seems a BNS issued trusted store is required.
+
+  Caused by: org.springframework.web.client.ResourceAccessException: I/O error on GET request for
+  "https://lb.vault.nonprod.bns:8200/v1/secret/data/token-pipeline-service-perimeter/uat/token-pipeline-mcopy/serviceAccount":
+  PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException:
+  unable to find valid certification path to requested target; nested exception is javax.net.ssl.SSLHandshakeException:
+  PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException:
+  unable to find valid certification path to requested target
+	at org.springframework.web.client.RestTemplate.doExecute(RestTemplate.java:785) ~[spring-web-5.3.4.jar:5.3.4]
+   */
+  private void readFromUAT() throws Exception {
+    VaultEndpoint vaultEndpoint = new VaultEndpoint();
+
+    vaultEndpoint.setHost("lb.vault.nonprod.bns");
+    vaultEndpoint.setPort(8200);
+    vaultEndpoint.setScheme("https");
+
+    VaultTemplate vaultTemplate = new VaultTemplate(
+        vaultEndpoint,
+        new TokenAuthentication(
+            "hvs.CAESIDj_ZDBApdo74WAckBUY9fumHT6hDUKQHAbBQlwyKmPAGiQKHGh2cy5obW04QklEa1FTWWhVNWptVldidHJPaVAQxYP6iAI")
+    );
+
+    Versioned<Map<String, Object>> readResponse =
+        vaultTemplate.opsForVersionedKeyValue("secret")
+            .get(
+                "token-pipeline-service-perimeter/uat/token-pipeline-mcopy/serviceAccount");
+
+    String password = "";
+    if (readResponse != null && readResponse.hasData()) {
+      password = (String) readResponse.getData().get("privateKey");
+    }
+
+    logger.info("Access granted: " + password);
+  }
 }
