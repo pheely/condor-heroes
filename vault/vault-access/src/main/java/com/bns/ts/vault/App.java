8ee390e2a19c10590931cbc9476e34d29577fa8d
Philip Yang <philip.j.yang@gmail.com>
2023-12-01 11:05:38 -0500

    vault access cleanup

--- vault/vault-access/src/main/java/com/bns/ts/vault/App.java
+++ vault/vault-access/src/main/java/com/bns/ts/vault/App.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.vault.authentication.TokenAuthentication;
-import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.support.VaultResponseSupport;
 import org.springframework.vault.support.Versioned;
@@ -17,9 +15,9 @@ import java.util.Map;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
+    Logger logger = LoggerFactory.getLogger(App.class);
     @Autowired
     private AppConfig appConfig;
-    Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String... args) {
         SpringApplication.run(App.class, args);
@@ -27,22 +25,28 @@ public class App implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        VaultEndpoint vaultEndpoint = new VaultEndpoint();
-//
-//        vaultEndpoint.setHost("0468-99-229-154-201.ngrok.io");
-//        vaultEndpoint.setPort(443);
-//        vaultEndpoint.setScheme("https");
+//    writeSecrets();
+        readSecrets();
+    }
 
-//        VaultTemplate vaultTemplate = new VaultTemplate(
-//                vaultEndpoint,
-//                new TokenAuthentication("hvs.CCOLpHinOR61cPg64nTRrG9y"));
-
-	    try {
+    private void readSecrets() {
         VaultTemplate vaultTemplate = new VaultTemplate(appConfig.vaultEndpoint(), appConfig.clientAuthentication());
-	/*
+        Versioned<Map<String, Object>> readResponse = vaultTemplate.opsForVersionedKeyValue("secret").get("top-secret");
+
+        String password = "";
+        if (readResponse != null && readResponse.hasData()) {
+            password = (String) readResponse.getData().get("password");
+        }
+        logger.info("Read secret: " + password);
+
+    }
+
+    private void writeSecrets() {
         Secrets secrets = new Secrets();
         secrets.userName = "hello";
         secrets.password = "world";
+
+        VaultTemplate vaultTemplate = new VaultTemplate(appConfig.vaultEndpoint(), appConfig.clientAuthentication());
 
         // read() and write() only work on kv-v1 which does not support versions
         vaultTemplate.write("secret-v1/my-secrets", secrets);
@@ -55,27 +59,21 @@ public class App implements CommandLineRunner {
         Versioned.Metadata createResponse = vaultTemplate.opsForVersionedKeyValue("secret").put("top-secrets", secrets);
         Versioned.Version version = createResponse.getVersion();
         logger.info("Secret written successfully - version: " + version.getVersion());
-        */
 
         Versioned<Map<String, Object>> readResponse = vaultTemplate.opsForVersionedKeyValue("secret").get("top-secret");
 
-	String password = "";
-	  if (readResponse != null && readResponse.hasData()) {
-      password = (String) readResponse.getData().get("password");
-    }
-    logger.info("Read secret: "+ password);
+        String password = "";
+        if (readResponse != null && readResponse.hasData()) {
+            password = (String) readResponse.getData().get("password");
+        }
+        logger.info("Read secret: " + password);
 
-    /*
         Map<String, Object> secrets2 = new HashMap<>();
         if (readResponse != null && readResponse.hasData()) {
             secrets2 = readResponse.getData();
         }
         logger.info("Read secrets: " + secrets2);
-*/
-//    vaultTemplate.opsForVersionedKeyValue("secret").delete("my-secrets", version);
-//    vaultTemplate.opsForVersionedKeyValue("secret").destroy("my-secrets", version);
-    }catch (Exception ex) {
-      ex.printStackTrace();
-    }
+        vaultTemplate.opsForVersionedKeyValue("secret").delete("my-secrets", version);
+        vaultTemplate.opsForVersionedKeyValue("secret").destroy("my-secrets", version);
     }
 }
